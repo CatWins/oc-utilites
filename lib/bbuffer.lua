f1b661d68463915e19d4d403d4238d18110c7a72
CatWins <MIVQWE@gmail.com>
2016-08-05 02:33:22 +0500

    OC update dirty bb fix

--- lib/bbuffer.lua
+++ lib/bbuffer.lua
@@ -1,12 +1,17 @@
 local com = require('component')
-local buffer = com.gt_batterybuffer
+local buffer = com.batterybuffer_09_tier_02
+local ic = com.inventory_controller
 
 local bb = {}
 
-local max = 400000  --single battery capacity
+--local max = 400000  --single battery capacity
+
+local battery = ic.getStackInSlot(0,1)
 
 function bb.getChargePercent()
-  local current = buffer.getBatteryCharge(1)
+  --local current = buffer.getBatteryCharge(1)
+  local max = battery.maxCharge
+  local current = battery.charge
   local progress = current / max * 100
   return math.floor(progress)
 end
